366d208bb0dc1d44f43a9f052894d68d6dec0afe
Alex <alexfrancisaaron@gmail.com>
2021-01-13 11:35:43 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Easy Does It", "Movie", 25000, "January 1, 2021", "February 1, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Ma Belle", "Movie", 50,000, "January 18, 2021", "February 18, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Hellevator", "Games", 10000, "January 3, 2021", "February 3, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Big Brother Public", "TV", 100000, "January 10, 2021", "February 10, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Top Chef College", "TV", 5000, "January 20, 2021", "February 20, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (6, "No Flip Puzzle", "Games", 5000, "January 19, 2021", "February 19, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (7, "Soul Mountain", "Movie", 25000, "January 1, 2021", "February 1, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (8, "Star Wars Prequel Series", "TV", 10000, "January 1, 2021", "February 1, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (9, "Not the Other Thing", "Movie", 15000, "January 3, 2021", "February 3, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (10, "The Fever", "Movie", 10000, "January 15, 2021", "February 15, 2021");
+
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Stephanie Cox", 33)
+INSERT INTO users (name, age) VALUES ("Ben Matheny", 30)
+INSERT INTO users (name, age) VALUES ("Bethany Jones", 31)
+INSERT INTO users (name, age) VALUES ("Kyle Aaron", 37)
+INSERT INTO users (name, age) VALUES ("Stanley Aaron", 41)
+INSERT INTO users (name, age) VALUES ("Stanley Aaron, Sr.", 65)
+INSERT INTO users (name, age) VALUES ("Will Addison", 28)
+INSERT INTO users (name, age) VALUES ("David Leblanc", 35)
+INSERT INTO users (name, age) VALUES ("Sean Price", 33)
+INSERT INTO users (name, age) VALUES ("Vickie Aaron", 65)
+INSERT INTO users (name, age) VALUES ("Lee Garcia", 28)
+INSERT INTO users (name, age) VALUES ("John Ryan", 32)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
+INSERT INTO users (name, age) VALUES ("Alex Aaron", 33)
