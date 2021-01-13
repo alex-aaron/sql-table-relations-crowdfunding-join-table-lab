eb8f08983808044174a91b3a3a7479feaa037f25
Alex <alexfrancisaaron@gmail.com>
2021-01-13 11:51:35 -0600

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Easy Does It", "Movie", 25000, "January 1, 2021", "February 1, 2021");
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Ma Belle", "Movie", 50,000, "January 18, 2021", "February 18, 2021");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (2, "Ma Belle", "Movie", 50000, "January 18, 2021", "February 18, 2021");
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (3, "Hellevator", "Games", 10000, "January 3, 2021", "February 3, 2021");
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (4, "Big Brother Public", "TV", 100000, "January 10, 2021", "February 10, 2021");
 INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (5, "Top Chef College", "TV", 5000, "January 20, 2021", "February 20, 2021");
@@ -14,18 +14,57 @@ INSERT INTO users (name, age) VALUES ("Stephanie Cox", 33);
 INSERT INTO users (name, age) VALUES ("Ben Matheny", 30);
 INSERT INTO users (name, age) VALUES ("Bethany Jones", 31);
 INSERT INTO users (name, age) VALUES ("Kyle Aaron", 37);
+
 INSERT INTO users (name, age) VALUES ("Stanley Aaron", 41);
 INSERT INTO users (name, age) VALUES ("Stanley Aaron, Sr.", 65);
 INSERT INTO users (name, age) VALUES ("Will Addison", 28);
 INSERT INTO users (name, age) VALUES ("David Leblanc", 35);
 INSERT INTO users (name, age) VALUES ("Sean Price", 33);
+
 INSERT INTO users (name, age) VALUES ("Vickie Aaron", 65);
 INSERT INTO users (name, age) VALUES ("Lee Garcia", 28);
 INSERT INTO users (name, age) VALUES ("John Ryan", 32);
 INSERT INTO users (name, age) VALUES ("Beal Locke", 27);
 INSERT INTO users (name, age) VALUES ("Sean Mahoney", 32);
+
 INSERT INTO users (name, age) VALUES ("Kat Donahoe", 35);
 INSERT INTO users (name, age) VALUES ("Buena Baptiste", 38);
 INSERT INTO users (name, age) VALUES ("Nathan Cooper", 31);
 INSERT INTO users (name, age) VALUES ("Desi Richter", 52);
 INSERT INTO users (name, age) VALUES ("Jonathan Perry", 26);
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (500, 1, 10)
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, 2, 9)
+INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 3, 8)
+INSERT INTO pledges (user_id, project_id) VALUES (4, 7)
+INSERT INTO pledges (user_id, project_id) VALUES (5, 6)
+
+INSERT INTO pledges (user_id, project_id) VALUES (6, 5)
+INSERT INTO pledges (user_id, project_id) VALUES (7, 4)
+INSERT INTO pledges (user_id, project_id) VALUES (8, 3)
+INSERT INTO pledges (user_id, project_id) VALUES (9, 2)
+INSERT INTO pledges (user_id, project_id) VALUES (10, 1)
+
+INSERT INTO pledges (user_id, project_id) VALUES (11, 10)
+INSERT INTO pledges (user_id, project_id) VALUES (12, 9)
+INSERT INTO pledges (user_id, project_id) VALUES (13, 8)
+INSERT INTO pledges (user_id, project_id) VALUES (14, 7)
+INSERT INTO pledges (user_id, project_id) VALUES (15, 6)
+
+INSERT INTO pledges (user_id, project_id) VALUES (16, 5)
+INSERT INTO pledges (user_id, project_id) VALUES (17, 4)
+INSERT INTO pledges (user_id, project_id) VALUES (18, 3)
+INSERT INTO pledges (user_id, project_id) VALUES (19, 2)
+INSERT INTO pledges (user_id, project_id) VALUES (20, 1)
+
+INSERT INTO pledges (user_id, project_id) VALUES (1, 10)
+INSERT INTO pledges (user_id, project_id) VALUES (2, 9)
+INSERT INTO pledges (user_id, project_id) VALUES (3, 8)
+INSERT INTO pledges (user_id, project_id) VALUES (4, 7)
+INSERT INTO pledges (user_id, project_id) VALUES (5, 6)
+
+INSERT INTO pledges (user_id, project_id) VALUES (6, 5)
+INSERT INTO pledges (user_id, project_id) VALUES (7, 4)
+INSERT INTO pledges (user_id, project_id) VALUES (8, 3)
+INSERT INTO pledges (user_id, project_id) VALUES (9, 2)
+INSERT INTO pledges (user_id, project_id) VALUES (10, 1)
